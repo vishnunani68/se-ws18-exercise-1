0a4a787142743ed47a69cbfa9c84e0164d397b03
vishnunani68 <44263050+vishnunani68@users.noreply.github.com>
2018-11-01 10:07:52 +0100

    Update Image.java

--- Image.java
+++ Image.java
@@ -21,29 +21,6 @@ import java.awt.*
  public Void MouseEntered (MouseEvent e){
      x = e.getx();
      y = e.gety();
-     str = "Mouse External";import java.awt.*
- public Class MouseEventHandler extends Frame implements
-   int x=0 , y=0 ;
-   String str = " ";
- public MouseEventHandler (){
-   setSize (100,100,200);
-   setTitle("mouse Event Test")
-  WindowHandler wh =
-       add windowListner (this);
-       add MouseListner  (this);
-  }
- public Void Paint (Graphics g){
-     g.draw String (str,x,y);
-  }
- public Void MouseClicked (MouseEvent e){
-     x = e.getx();
-     y = e.gety();
-     str = "Mouse Clicked";
-      repaint ();
-  }
- public Void MouseEntered (MouseEvent e){
-     x = e.getx();
-     y = e.gety();
      str = "Mouse External";
       repaint ();
   }
@@ -68,8 +45,16 @@ import java.awt.*
  public static void main (String args[]){
     MouseEventHandler meh = new MouseEventHandler ();
                       meh.setvisible (true);
+ public Void MouseEvented (MouseEvent e){
+     x = e.getx();
+     y = e.gety();
+   str = "Mouse Clicked";
+      repaint ();
   }
-}
+  public Void MouseEvented (MouseEvent e){
+     x = e.getx();
+     y = e.gety();
+     str = "Mouse External";
       repaint ();
   }
  public Void MouseEvented (MouseEvent e){
